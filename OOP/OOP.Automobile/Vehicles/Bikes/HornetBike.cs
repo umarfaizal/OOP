51d94d3d916fa35d71448a6cb047126af447569b
vignesh-nethaji <vigneshnethajichengam@gmail.com>
2021-08-05 12:45:26 +0530

    Added implicit and explicit operator loading for Hornet Bike

--- OOP/OOP.Automobile/Vehicles/Bikes/HornetBike.cs
+++ OOP/OOP.Automobile/Vehicles/Bikes/HornetBike.cs
@@ -8,10 +8,23 @@ namespace OOP.Automobile.Vehicles.Bikes
 {
     public sealed class HornetBike : Bike, IBike
     {
+        public HornetBike(string name) : base(name, Brand.Hyundai, 188, true)
+        {
+
+        }
         public HornetBike() : base("Hornet", Brand.Hyundai, 188, true)
         {
             PSVersion = 6;
             Console.WriteLine($"The PS version of {Name} is {PSVersion}");
+        }
+
+        public static implicit operator string(HornetBike bike)
+        {
+            return bike.Name;
+        }
+        public static explicit operator HornetBike(string name)
+        {
+            return new HornetBike(name);
         }
     }
 }
