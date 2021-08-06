1db1d1783ec1de11a3497b5f4ec0d61a4b0901a7
vignesh-nethaji <vigneshnethajichengam@gmail.com>
2021-08-06 11:28:42 +0530

    Car File as split two file CarInit and CarFields for partial class definition

---
+++ OOP/OOP.Automobile/Vehicles/Cars/CarFields.cs
@@ -0,0 +1,38 @@
+﻿using OOP.Automobile.Enums;
+using OOP.Automobile.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP.Automobile.Vehicles.Cars
+{
+    public partial class Car : Vehicle, ICar
+    {
+        public override VehicleType TypeOfVehicle
+        {
+            get
+            {
+                return _typeofVehicle;
+            }
+            set
+            {
+                throw new ArgumentException("Already vehicle type defined as Car. Don't try to set it");
+            }
+        }
+
+        public bool IsAcAvailable { get; set; }
+        public bool IsSunRoofAvailable { get; set; }
+
+        public sealed override int NumberOfWheels
+        {
+            get
+            {
+                return _numberOfWheels;
+            }
+            set
+            {
+                throw new ArgumentException("Car wheels already defined we can't it");
+            }
+        }
+    }
+}
