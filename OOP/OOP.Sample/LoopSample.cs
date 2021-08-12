f3d0c0c41cdd50a01167fca7b3fff8e0a4fd08c8
vignesh-nethaji <vigneshnethajichengam@gmail.com>
2021-08-12 14:04:04 +0530

    Comments loops

--- OOP/OOP.Sample/LoopSample.cs
+++ OOP/OOP.Sample/LoopSample.cs
@@ -8,6 +8,7 @@ namespace OOP.Sample
     {
         public void DoWhile()
         {
+            // Do While code block will call atleast one time in the execution , after that only it will check the condition
             int i = 10;
             do
             {
@@ -18,6 +19,7 @@ namespace OOP.Sample
         public void While()
         {
             int i = 10;
+            // while loop first check condition before executing code block , if it true then only code block will execute.
             while (i < 10)
             {
                 Console.WriteLine("While Calling");
